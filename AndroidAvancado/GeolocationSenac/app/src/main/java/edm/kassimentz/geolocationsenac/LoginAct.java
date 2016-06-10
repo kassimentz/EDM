fcdd75f7f58e819947a8107424f68b5bb8b5c1ab
Kassi <kassimentz@gmail.com>
2016-06-09 22:55:02 -0300

    update geolocationSenac

--- AndroidAvancado/GeolocationSenac/app/src/main/java/edm/kassimentz/geolocationsenac/LoginAct.java
+++ AndroidAvancado/GeolocationSenac/app/src/main/java/edm/kassimentz/geolocationsenac/LoginAct.java
@@ -19,7 +19,7 @@ public class LoginAct extends AppCompatActivity {
    @Override
    protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
-       setContentView(R.layout.activity_login);
+       setContentView(R.layout.act_login);
 
        callbackManager = CallbackManager.Factory.create();
 
