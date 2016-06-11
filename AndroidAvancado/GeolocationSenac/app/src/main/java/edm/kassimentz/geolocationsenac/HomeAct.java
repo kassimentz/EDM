38a281e022ac4516deb6241e498556dad8e21ba0
Kassiane Mentz <kassimentz@gmail.com>
2016-06-11 15:00:05 -0300

    add maps

--- AndroidAvancado/GeolocationSenac/app/src/main/java/edm/kassimentz/geolocationsenac/HomeAct.java
+++ AndroidAvancado/GeolocationSenac/app/src/main/java/edm/kassimentz/geolocationsenac/HomeAct.java
@@ -1,8 +1,17 @@
 package edm.kassimentz.geolocationsenac;
 
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -10,9 +19,11 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class HomeAct extends FragmentActivity implements OnMapReadyCallback {
+public class HomeAct extends FragmentActivity implements OnMapReadyCallback,
+        GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener {
 
     private GoogleMap mMap;
+    private GoogleApiClient mGoogleApiClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -39,8 +50,63 @@ public class HomeAct extends FragmentActivity implements OnMapReadyCallback {
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
+        /*LatLng sydney = new LatLng(-34, 151);
+
+        MarkerOptions mOpt = new MarkerOptions();
+        mOpt.position(sydney);
+        mOpt.title("Marker Sidney");
+        mOpt.snippet("Cidade da Austrália");
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));*/
+
+        // Create an instance of GoogleAPIClient.
+        if (mGoogleApiClient == null) {
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addConnectionCallbacks(this) //ConnectionsCallBack
+                    .addOnConnectionFailedListener(this)
+                    .addApi(LocationServices.API)
+                    .build();
+            mGoogleApiClient.connect();
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    @Override
+    public void onConnected(Bundle bundle) {
+        //o client conectou com o play service
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            return;
+        }
+        showMe();
+    }
+
+    private void showMe() {
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        LatLng eu = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+        mMap.addMarker(new MarkerOptions().position(eu).title("Estou aqui"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(eu, 16));
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if(requestCode == 1){
+            if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                showMe();
+            }
+        }
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+        //conexao suspensa
+    }
+
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
+        // falha na comunicacao
     }
 }
