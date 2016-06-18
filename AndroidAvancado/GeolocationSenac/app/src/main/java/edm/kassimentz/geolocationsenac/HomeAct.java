9d7e5abcd153fedb972379befc420799dd39b0e5
Kassiane Mentz <kassimentz@gmail.com>
2016-06-18 11:05:11 -0300

    update geolocation working on emulator

--- AndroidAvancado/GeolocationSenac/app/src/main/java/edm/kassimentz/geolocationsenac/HomeAct.java
+++ AndroidAvancado/GeolocationSenac/app/src/main/java/edm/kassimentz/geolocationsenac/HomeAct.java
@@ -200,7 +200,7 @@ public class HomeAct extends FragmentActivity implements OnMapReadyCallback,
     public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
         if(requestCode == 1){
             if(grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
-                showMe();
+              //  showMe();
             }
         }
     }
