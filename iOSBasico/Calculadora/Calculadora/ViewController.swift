d6f566ce55fe3089f68245242193adea462895cd
iossenac <iossenac@SALA309PC16.local>
2016-09-03 16:10:53 -0300

    first ios project - calculadora

---
+++ iOSBasico/Calculadora/Calculadora/ViewController.swift
@@ -0,0 +1,54 @@
+//
+//  ViewController.swift
+//  Calculadora
+//
+//  Created by iossenac on 03/09/16.
+//  Copyright © 2016 Kassiane Mentz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    var brain  = CalculatorBrain()
+    var userStartedToType: Bool = false
+    
+    @IBOutlet weak var display: UILabel!
+
+    @IBAction
+    func digitPressed(sender: UIButton) {
+        
+        let digit = sender.currentTitle!
+        
+        if userStartedToType{
+            display.text = display.text! + digit
+        }else{
+            display.text = digit
+        }
+        userStartedToType = true
+    }
+    
+    var displayText: Double {
+        get {
+            return Double(display.text!)!
+        }
+        set{
+            display.text = String(newValue)
+        }
+    }
+
+    @IBAction func executeOperation(sender: UIButton) {
+        
+        let operation = sender.currentTitle!
+        
+        if let value = Double(display.text!){
+            brain.setOperand(value)
+            brain.execute(operation)
+            displayText = brain.accumulator
+        }
+        
+        userStartedToType = false
+    }
+
+}
+
