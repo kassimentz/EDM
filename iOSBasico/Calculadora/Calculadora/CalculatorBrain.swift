c7d9a46223cef0fb1672ee19bbd732062920ab9f
iossenac <iossenac@SALA309PC16.local>
2016-09-10 13:51:22 -0300

    update calculadora brain

--- iOSBasico/Calculadora/Calculadora/CalculatorBrain.swift
+++ iOSBasico/Calculadora/Calculadora/CalculatorBrain.swift
@@ -24,22 +24,18 @@ class CalculatorBrain
         case Clear
     }
     
-    
-    func selectOperation(operation: String) -> Operation{
-        switch operation {
-        case "π": return Operation.Constant(M_PI)
-        case "√": return Operation.UnaryOperation(sqrt)
-        case "cos": return Operation.UnaryOperation(cos)
-        case "sen": return Operation.UnaryOperation(sin)
-        case "×": return Operation.BinaryOperation({$0 * $1})
-        case "÷": return Operation.BinaryOperation({$0 / $1})
-        case "+": return Operation.BinaryOperation({$0 + $1})
-        case "-": return Operation.BinaryOperation({$0 - $1})
-        case "=": return Operation.Equals
-        case "clr": return Operation.Clear
-        default: return Operation.Equals
-        }
-    }
+    let operations : [String:Operation] = [
+        "π": Operation.Constant(M_PI),
+        "√": Operation.UnaryOperation(sqrt),
+        "cos": Operation.UnaryOperation(cos),
+        "sen": Operation.UnaryOperation(sin),
+        "×": Operation.BinaryOperation({$0 * $1}),
+        "÷": Operation.BinaryOperation({$0 / $1}),
+        "+": Operation.BinaryOperation({$0 + $1}),
+        "-": Operation.BinaryOperation({$0 - $1}),
+        "=": Operation.Equals,
+        "clr": Operation.Clear
+    ]
     
     struct BinaryOperationInfo {
         var operand: Double
@@ -52,18 +48,21 @@ class CalculatorBrain
     
     var binaryOperationInfo: BinaryOperationInfo?
     
-    func execute(operation: String){
-        switch self.selectOperation(operation) {
-        case .Constant(let value): accumulator = value
-        case .UnaryOperation(let function): accumulator = function(accumulator)
-        case .BinaryOperation(let function):
-            binaryOperationInfo = BinaryOperationInfo(operand: accumulator, operation: function)
-        case .Equals: if let op = binaryOperationInfo{
+    func execute(symbol: String){
+        if let operation = operations[symbol] {
+            switch operation {
+            case .Constant(let value): accumulator = value
+            case .UnaryOperation(let function): accumulator = function(accumulator)
+            case .BinaryOperation(let function):
+                binaryOperationInfo = BinaryOperationInfo(operand: accumulator, operation: function)
+            case .Equals: if let op = binaryOperationInfo{
                 accumulator = op.exec(accumulator)
+                }
+            case .Clear:
+                accumulator = 0
+                binaryOperationInfo = nil
             }
-        case .Clear:
-            accumulator = 0
-            binaryOperationInfo = nil
         }
+        
     }
 }
